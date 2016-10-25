eac9bf94dd030998497d9d248333d2c9c6f91b0d
brendan <bobra@piiq.co>
2016-10-25 13:35:44 -0700

    adding message processor

---
+++ src/main/scala/org/obrafamily/mavlink/MavlinkParser.scala
@@ -0,0 +1,31 @@
+package org.obrafamily.mavlink
+
+import _root_.org.mavlink.messages.Message
+import _root_.org.mavlink.{Packet, Parser}
+import akka.util.ByteString
+
+/**
+  * Created by brendan on 10/25/16.
+  */
+class MavlinkParser(data:ByteString) {
+  val parser = new Parser(
+    (pckt: Packet) => {
+      val msg: Message = Message.unpack(pckt.messageId, pckt.payload)
+      msg
+    },
+    (err: Parser.Errors.Error) => {
+      sys.error("parse error: " + err)
+    }
+  )
+
+  def toMessage() = {
+    Packet.apply()
+
+    parser.push(data)
+  }
+}
+object  MavlinkParser{
+  def apply(data:ByteString) = {
+    new MavlinkParser(data)
+  }
+}
